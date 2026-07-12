7c32db688ccb588b9dc5dc26550fb39903429eae
Saurav Mishra <mishrasaurav2080@gmail.com>
2026-07-12 12:42:03 +0545

    Completed homepage layout with hero, packages, and destinations next to do is remaining section in home page such as testimonials and others

--- trek_sathi/urls.py
+++ trek_sathi/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
    # path('', include('accounts.urls')),
     #path('companies/', include('companies.urls')),
     # path('packages/', include('packages.urls')),
